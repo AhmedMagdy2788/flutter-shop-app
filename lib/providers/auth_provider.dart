7fb5d38d60254e621b3781dcd96c20502b5a80e8
AhmedMagdy2788 <Ahmed.Magdy.2788@gmail.com>
2019-11-26 15:05:21 +0200

    update return

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
+
 import '../models/http_exception.dart';
 
 class AuthProvider with ChangeNotifier {
